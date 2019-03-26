7799b9952bb92cec35bf15f631a8f31c1331f697
nicolas oravian <nicolas@oravian.com>
2019-03-26 15:53:11 +0100

    rnredux create

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import { StackNavigator } from 'react-navigation'
+
+import store from './store'
+import MainScreen from './screens/MainScreen'
+
+export default class App extends Component {
+  render() {
+    const MainNavigator = StackNavigator({
+      Main: { screen: MainScreen },
+      Login: { screen: LoginScreen },
+    })
+
+    return (
+      <Provider store={store}>
+        <MainNavigator />
+      </Provider>
+      )
+  }
+}
---
+++ screens/MainScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { View, Platform } from 'react-native'
+import { STATUS_BAR_HEIGHT } from '../constants'
+
+export default class MainScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: 'orange' }} />
+    )
+  }
+}
